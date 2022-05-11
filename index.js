bc26d30475b8495d62f44c94ff619f24caf8aeab
Lubpui <98448740+Lubpui@users.noreply.github.com>
2022-05-11 17:53:09 +0700

    index finish & Check นิดหน่อย

--- index.js
+++ index.js
@@ -5,3 +5,24 @@ const bodyParser = require('body-parser')
 const app = express()
 const port = process.env.PORT || 1039;
 
+const pool = mysql.createConnection({
+    host : 'localhost', 
+    user : 'root',
+    password : '',
+    database : 'nodejs_lottery'
+})
+
+app.set('view engine','ejs')
+
+app.use(express.static('public'))
+app.use(bodyParser.urlencoded({extended: false})) 
+
+app.get('/',(req,res)=>{
+    res.render('index')
+})
+
+app.get('/checkLott',(req,res)=>{
+    res.render('checkLott')
+})
+
+app.listen(port,() => console.log("Server Post :",port,"Is Running..."))
